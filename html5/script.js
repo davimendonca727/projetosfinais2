0466f8eea1175dfcbaeaf9c31c1dabdd19458c0c
davimendonca727 <davimendoncadossantos008@gmail.com>
2025-06-13 11:51:16 -0300

    Delete html5 directory

--- html5/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const sidebar = document.getElementById('sidebar');
-
-    menuToggle.addEventListener('click', () => {
-        sidebar.classList.toggle('active');
-    });
-});
